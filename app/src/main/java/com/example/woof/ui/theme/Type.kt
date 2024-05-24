9820f572b8255399dd79d60961d009f8f61a5b8a
kakudji simba <21ks128@esisalama.org>
2024-05-24 09:26:50 +0200

    ajout des elements dans Type

--- app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.woof.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
+import androidx.compose.ui.text.font.FontFamily
+import com.example.woof.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,6 +18,9 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +37,12 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
 )
